df09dd4e12479fe675c5534561b11128d9534de2
Gustavo Zille <guzillaine@gmail.com>
2026-01-30 22:08:47 -0300

    enemy spawner improved

--- enemies/enemy_spawner.gd
+++ enemies/enemy_spawner.gd
@@ -1,9 +1,19 @@
 extends Node3D
 
-@onready var enemy_scene = preload("res://enemies/Enemy.tscn")
+@export var wait_time = 4.0
 
-func _on_timer_timeout() -> void:
+@onready var enemy_scene = preload("res://enemies/Enemy.tscn")
+@onready var timer = $Timer
+@onready var spawn_points = $SpawnPoints
+
+func _ready():
+	timer.wait_time = wait_time
+
+func get_random_spawn_position():
+	return spawn_points.get_children().pick_random().global_position
+
+func _on_timer_timeout():
 	var enemy_instance = enemy_scene.instantiate()
 	enemy_instance.type = randi() % game_state.types_of_masks.size()
-	enemy_instance.global_position = global_position
 	get_parent().add_child(enemy_instance)
+	enemy_instance.global_position = get_random_spawn_position()
