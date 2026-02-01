e0992f2ccb9387cbb1b6125dc3c1bd66a2165afb
Gustavo Zille <guzillaine@gmail.com>
2026-02-01 15:57:13 -0300

    basic main menu

---
+++ menus/main_menu.gd
@@ -0,0 +1,13 @@
+extends Node3D
+
+func _on_play_button_pressed():
+	get_tree().change_scene_to_file("res://World.tscn")
+
+func _on_controls_button_pressed():
+	pass # Replace with function body.
+
+func _on_credits_button_pressed():
+	pass # Replace with function body.
+
+func _on_exit_button_pressed():
+	get_tree().quit()
